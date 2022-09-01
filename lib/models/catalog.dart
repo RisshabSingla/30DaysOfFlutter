ac470a45be0e3947ba646d3920b0e6bbb14823d5
Risshab Singla <risshab2singla@gmail.com>
2022-09-01 16:52:59 +0530

    Day 12 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -23,7 +23,7 @@ class CatalogModel {
         description: "Apple iPhone 12th Generation",
         price: 999,
         color: "#33505a",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkRNmXnENOc58p4BFvUnpm4MqmKuhM75GpQg&usqp=CAU"
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"
     )
   ];
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,37 +1,57 @@
 import 'package:android_studio_projects/models/catalog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'dart:convert';
+// gives json encoder and decoder
 
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-  final int days = 30;
-  final String name = "Risshab";
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // before the builder
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodeJson = jsonDecode(catalogJson); // gives a map
+    // print(catalogJson);
+    var productData = decodeJson["products"];
+
+  }
 
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     // generates a dummy list in order to compensate for lack of data
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Catalog App",
-          ),
+      appBar: AppBar(
+        title: const Text(
+          "Catalog App",
         ),
-
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-
-          child: ListView.builder(
-            itemCount: dummyList.length,    //CatalogModel.items.length,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length, //CatalogModel.items.length,
             itemBuilder: (context, index) {
               return ItemWidget(
-                  item: dummyList[index]    //CatalogModel.items[index]
-              );
-            }
-    ),
-        ),
-    drawer : myDrawer(),
+                  item: dummyList[index] //CatalogModel.items[index]
+                  );
+            }),
+      ),
+      drawer: myDrawer(),
     );
   }
 }
