0a0646f032fa7edb9b3536ff2ec43a3aebc30e7a
Risshab Singla <risshab2singla@gmail.com>
2022-09-01 20:54:58 +0530

    Day 14 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -50,14 +50,51 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items !=null && CatalogModel.items.isNotEmpty)? ListView.builder(
-            itemCount: CatalogModel.items.length, //CatalogModel.items.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(
-                  item: CatalogModel.items[index] //CatalogModel.items[index]
-                  );
-            },
-        ):Center(child: CircularProgressIndicator()),
+        child: (CatalogModel.items !=null && CatalogModel.items.isNotEmpty)
+            ?GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+            ),
+            itemBuilder: (context,index)
+          {final item = CatalogModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              child: GridTile(
+                header: Container(
+                    child: Text(item.name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                    ),
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                  child: Image.network(item.image),
+                  footer: Container(
+                    child: Text(item.price.toString(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                    ),
+                  ),
+              ),
+
+            );
+          },
+          itemCount: CatalogModel.items.length,
+        )
+        :Center(child: CircularProgressIndicator()),
       ),
       drawer: myDrawer(),
     );
